075ac81c4e60d730685168c1413d11719230c0d2
Kacper Wójtowicz <kacper.m.wojtowicz@gmial.com>
2020-11-11 16:18:18 +0100

    31.4

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -11,4 +11,9 @@ router.get('/google/callback', passport.authenticate('google', { failureRedirect
   }
 );
 
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
 module.exports = router;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,12 +2,32 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/logged', (req, res) => {
-  res.render('logged');
+
+  if (req.user) {
+    res.render('logged', { username: req.user.displayName, image: req.user.photos[0].value });
+  } else {
+    res.redirect('/user/no-permission');
+  }
 });
 
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
 });
 
+router.get('/profile', (req, res) => {
+  if (req.user) {
+    res.render('profile');
+  } else {
+    res.redirect('/user/no-permission');
+  }
+});
+
+router.get('/profile/settings', (req, res) => {
+  if (req.user) {
+    res.render('settings');
+  } else {
+    res.redirect('/user/no-permission');
+  }
+});
 
 module.exports = router;
